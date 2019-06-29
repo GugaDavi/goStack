d21ee39df6544f7ef1adfe8385e904712a7bd5d8
GugaDavi <gguedes.tech@gmail.com>
2019-06-28 23:55:10 -0300

    SessionController

---
+++ goBarber/src/app/controllers/SessionController.js
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import auth from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Formato dos dados invalidos' });
+    }
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      res.status(401).json({ error: 'Usuario não cadastrado' });
+    }
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Possword invalido' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, auth.secret, {
+        expiresIn: auth.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
