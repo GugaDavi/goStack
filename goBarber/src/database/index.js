463ed891a535e47988f4ceda85d7f71dde6c2d0d
GugaDavi <gguedes.tech@gmail.com>
2019-07-05 02:56:27 -0300

    Add MongoDB e filtro dos models

--- goBarber/src/database/index.js
+++ goBarber/src/database/index.js
@@ -1,20 +1,37 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointments from '../app/models/Appointments';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointments];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    try {
+      this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
